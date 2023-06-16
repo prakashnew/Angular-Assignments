574f646b182c4fab8f61fc070a46f0ad5b0f0802
prakashnew <134586332+prakashnew@users.noreply.github.com>
2023-06-16 10:19:59 +0530

    Update header-image.component.ts

--- Exercise2/src/app/app/header-image/header-image.component.ts
+++ Exercise2/src/app/app/header-image/header-image.component.ts
@@ -1,1 +1,20 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-header-image',
+  templateUrl: './header-image.component.html',
+  styleUrls: ['./header-image.component.css']
+})
+export class HeaderImageComponent implements OnInit {
+  lImagePath="/assets/images/L.png"
+  leftImg = this.lImagePath;
+  rImagePath="/assets/images/R.png"
+  rightImg = this.rImagePath;
+  dotsImagePath="/assets/images/dots (1).png"
+  dotsImg = this.dotsImagePath;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
