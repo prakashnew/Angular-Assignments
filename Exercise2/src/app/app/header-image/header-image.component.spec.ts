c71ddfb73990b49bb70bbe51f1fb042668264827
prakashnew <134586332+prakashnew@users.noreply.github.com>
2023-06-16 10:20:15 +0530

    Update header-image.component.spec.ts

--- Exercise2/src/app/app/header-image/header-image.component.spec.ts
+++ Exercise2/src/app/app/header-image/header-image.component.spec.ts
@@ -1,1 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HeaderImageComponent } from './header-image.component';
+
+describe('HeaderImageComponent', () => {
+  let component: HeaderImageComponent;
+  let fixture: ComponentFixture<HeaderImageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeaderImageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HeaderImageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
