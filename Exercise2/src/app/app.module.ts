9aea8c4a26e0db2efd44231c1dc6e2a282d19461
prakashnew <134586332+prakashnew@users.noreply.github.com>
2023-06-16 10:08:17 +0530

    Update app.module.ts

--- Exercise2/src/app/app.module.ts
+++ Exercise2/src/app/app.module.ts
@@ -1,1 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuBarComponent } from './app/menu-bar/menu-bar.component';
+import { HeaderImageComponent } from './app/header-image/header-image.component';
+import { ContentBoxComponent } from './app/content-box/content-box.component';
+import { MainContentComponent } from './app/main-content/main-content.component';
+import { GalleryComponent } from './app/gallery/gallery.component';
+import { FooterComponent } from './app/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuBarComponent,
+    HeaderImageComponent,
+    ContentBoxComponent,
+    MainContentComponent,
+    GalleryComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
