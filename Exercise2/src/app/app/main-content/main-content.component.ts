bfd4d0286820f2e16bbfdd18842c4e01dd416f9f
prakashnew <134586332+prakashnew@users.noreply.github.com>
2023-06-16 10:22:04 +0530

    Update main-content.component.ts

--- Exercise2/src/app/app/main-content/main-content.component.ts
+++ Exercise2/src/app/app/main-content/main-content.component.ts
@@ -1,1 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { contentBox } from 'src/app/models/models';
 
+@Component({
+  selector: 'app-main-content',
+  templateUrl: './main-content.component.html',
+  styleUrls: ['./main-content.component.css']
+})
+export class MainContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  displayContents:contentBox[]=[
+    {title:"Content",content:"Lorem ipsum dolor sit amet;consectetuer adipiscing elit.sed diam nanummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat"},
+    {title:"Sub Header",content:"Lorem ipsum dolor sit amet;consectetuer adipiscing elit.sed diam nanummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat;consectetuer adipiscing elit.sed diam nanummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat"}    
+  ]
+  navigations:string[]=["Home","Product","Company","Contact"]
+}
