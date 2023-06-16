2606e1499e4a4b7ceb6d785e072ccf2b0c475e26
prakashnew <134586332+prakashnew@users.noreply.github.com>
2023-06-16 10:08:38 +0530

    Update app.component.ts

--- Exercise2/src/app/app.component.ts
+++ Exercise2/src/app/app.component.ts
@@ -1,1 +1,13 @@
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularproject1';
+  // imagePath="/assets/images/img1.png"
+  // img = this.imagePath;
+  
+}
