8e3aea8d7efef8f5e45007a9b5cc43383ea34fb5
prakashnew <134586332+prakashnew@users.noreply.github.com>
2023-06-16 10:24:15 +0530

    Update menu-bar.component.ts

--- Exercise2/src/app/app/menu-bar/menu-bar.component.ts
+++ Exercise2/src/app/app/menu-bar/menu-bar.component.ts
@@ -1,1 +1,18 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.css']
+})
+export class MenuBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  // menu-bar array
+  menus:string[]=["Home","Product","Company","Contact"]
+  imagePath="/assets/images/fb.png"
+  fbImg = this.imagePath;
+}
