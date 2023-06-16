0d047c28c256a213f39cb81bddd6bc98ce2530d8
prakashnew <134586332+prakashnew@users.noreply.github.com>
2023-06-16 10:13:19 +0530

    Update content-box.component.ts

--- Exercise2/src/app/app/content-box/content-box.component.ts
+++ Exercise2/src/app/app/content-box/content-box.component.ts
@@ -1,1 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { contentBox } from 'src/app/models/models';
 
+@Component({
+  selector: 'app-content-box',
+  templateUrl: './content-box.component.html',
+  styleUrls: ['./content-box.component.css']
+})
+export class ContentBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  displayContents:contentBox[]=[
+    {title:"About",content:"Lorem ipsum dolor sit amet;consectetuer adipiscing elit.sed diam nanummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat"},
+    {title:"Company",content:"Lorem ipsum dolor sit amet;consectetuer adipiscing elit.sed diam nanummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat"},
+    {title:"Services",content:"Lorem ipsum dolor sit amet;consectetuer adipiscing elit.sed diam nanummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat"},
+  ]
+}
